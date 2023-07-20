4eb5c4e5ec1cdd636ef1f52b6c326291756fdeed
Julio Paredes <ing.julio.code@gmail.com>
2023-07-20 11:25:29 -0600

    creating the first endpoint for home

--- handlers/home.go
+++ handlers/home.go
@@ -1,6 +1,26 @@
 package handlers
 
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/kisstc/go-backend-template/server"
+)
+
+// se encargara de procesar la peticion de la ruta principal /
+// los mensajes que le enviaremos al cliente
 type HomeResponse struct {
-	Message string `json:"message"`
-	Status  bool   `json:"status"`
+	Message string `json:"message"` // serializamos a json, tomara el nombre en minuscula
+	Status  bool   `json:"status"`  // serializamos a json
+}
+
+func HomeHandler(s server.Server) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(HomeResponse{
+			Message: "Welcome to Ganadores",
+			Status:  true,
+		})
+	}
 }
---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+	"github.com/kisstc/go-backend-template/handlers"
+	"github.com/kisstc/go-backend-template/server"
+)
+
+func main() {
+	// cargar variables de entorno
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading env file")
+	}
+
+	PORT := os.Getenv("PORT")
+	JWT_SECRET := os.Getenv("JWT_SECRET")
+	DATABASE_URL := os.Getenv("DATABASE_URL")
+
+	// creamos servidor nuevo
+	s, err := server.NewServer(context.Background(), &server.Config{
+		Port:        PORT,
+		JWTSecret:   JWT_SECRET,
+		DatabaseUrl: DATABASE_URL,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	s.Start(BindRoutes)
+
+}
+
+// funcion bind para start server
+func BindRoutes(s server.Server, r *mux.Router) {
+	r.HandleFunc("/", handlers.HomeHandler(s)).Methods(http.MethodGet)
+}
