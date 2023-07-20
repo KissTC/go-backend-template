a2a6d2b96deea322995faa56135598e70f0d29e2
Julio Paredes <ing.julio.code@gmail.com>
2023-07-20 12:09:30 -0600

    user model sample

---
+++ models/user.go
@@ -0,0 +1,11 @@
+package models
+
+// en modelos le decimos a nivel de Go como funciona el flujo de datos
+// como funcionan los modelos, que tipos usan, como se integran
+
+// modelo de usuario a nivel de Go (se tiene que ver a nivel de go, patron de diseño, bd, etc)
+type User struct {
+	Id       int64  `json:"id"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
